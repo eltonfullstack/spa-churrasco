f398d02e8886856b0d83c3edda5021910a2975af
Elton Silva <eltonsilva@Eltons-MacBook-Pro.local>
2024-05-26 23:39:27 -0300

    basic layout

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+
+const Banner = () => {
+
+    return (
+        <>
+            <div className="container-banner-title">
+              <h1 className="banner-title">Churrasco dos Homens</h1>
+              <p>com futebol</p>
+            </div>
+        </>
+    )
+}
+
+export default Banner;
