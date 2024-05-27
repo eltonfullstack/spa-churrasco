b2dbf91fb2558a4af1522732e53bff58f357ed98
Elton Silva <eltonsilva@Eltons-MacBook-Pro.local>
2024-05-27 09:32:59 -0300

    Layout feito

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Banner from "./components/Banner"
 import calendar from './assets/calendar_black.png'
 import wallet from './assets/wallet_black.png'
 import clock from './assets/clock.png'
+import soccer from './assets/soccer.png'
+import barbecue from './assets/barbecue.png'
+import map_location from './assets/map_location.png'
 
 function App() {
 
@@ -12,6 +15,16 @@ function App() {
         <Banner />
       </div>
       <div className="container">
+
+        <div className="versiculo">
+          <h1 id="versiculo-titulo">Provérbios 16:21-24</h1>
+          <p>
+            O homem sábio se torna famoso pelo seu bom senso; quem fala com equilíbrio 
+            promove o ensino. A sabedoria é a fonte da vida para os sábios, mas o 
+            tolo será castigado pela sua própria tolice. O coração do homem 
+            sábio controla suas palavras e os seus lábios promovem a instrução.
+          </p>
+        </div>
 
         <div className="container-card">
           <div className="card">
@@ -30,6 +43,25 @@ function App() {
             <p>R$ 50,00</p>
           </div>
         </div>
+
+        <div className="container-card">
+          <div className="card">
+            <img src={map_location} alt="wallet" />
+            <h1>Local</h1>
+            <p>Rua Brejaúva, 337 -  Diadema</p>
+          </div>
+
+          <div className="card">
+            <img src={soccer} alt="calendar" />
+            <h1>Futebol</h1>
+            <p>Das 15:00 às 16:30</p>
+          </div>
+          <div className="card">
+            <img src={barbecue} alt="clock" />
+           <h1>Churrasqueira</h1>
+           <p>Número 07</p>
+          </div>
+        </div>
         
           <div className="container-mais-infos">
             <div className="div-mais-local"></div>
@@ -39,8 +71,8 @@ function App() {
 
         <div className="container-images">
             <div className="img-00">
-              <h1>Churrasco dos Homens</h1>
-              <p>Entre 13:00 e 22:00</p>
+              <h1 id="alerta-pagamaento">Pagamentos até 05/06/2024</h1>
+              <p>Precisamos providenciar as coisas com antecedência</p>
             </div>
             <div className="img-01">
             </div>
@@ -73,10 +105,12 @@ function App() {
             </div>
             <div className="logo-pix">
               <h2 className="participantes-titulo">Pagamento</h2>
-              <p>PIX 11940266961</p>
+              <p>PIX 11940266961 - <span id="nubank">Nubank</span></p>
+              
               <strong>Alessandro Dias da Silva</strong>
             </div>
           </div>
+          
       </div>
     </>
   )
